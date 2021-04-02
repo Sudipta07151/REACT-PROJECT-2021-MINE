49493e8e90e7ec2ce83e64a5abf93a9ccd4534ea
Sudipta Adak <adak07151@gmail.com>
2021-04-03 02:57:29 +0530

    googlebooksapi connected success

--- src/components/InsertNew.js
+++ src/components/InsertNew.js
@@ -1,95 +1,32 @@
-import React from 'react';
-import { makeStyles, fade } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import Container from '@material-ui/core/Container';
-import SearchIcon from '@material-ui/icons/Search';
-import InputBase from '@material-ui/core/InputBase';
+import React, { useState } from 'react';
+import SearchArea from '../components/reusable/SearchArea';
+import BookCard from '../components/reusable/BookCard'
+import axios from 'axios';
 
-
-const useStyles = makeStyles((theme) => ({
-    root: {
-        flexGrow: 1,
-    },
-    title: {
-        display: 'none',
-        [theme.breakpoints.up('sm')]: {
-            display: 'block',
-        },
-    },
-    search: {
-        position: 'relative',
-        borderRadius: theme.shape.borderRadius,
-        backgroundColor: fade(theme.palette.common.white, 0.15),
-        '&:hover': {
-            backgroundColor: fade(theme.palette.common.white, 0.25),
-        },
-        marginRight: theme.spacing(2),
-        marginLeft: 0,
-        width: '100%',
-        [theme.breakpoints.up('sm')]: {
-            marginLeft: theme.spacing(3),
-            width: 'auto',
-        },
-    },
-    appbar: {
-        backgroundColor: "transparent"
-    },
-    searchIcon: {
-        padding: theme.spacing(0, 2),
-        height: '100%',
-        position: 'absolute',
-        pointerEvents: 'none',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    inputRoot: {
-        color: 'inherit',
-    },
-    inputInput: {
-        padding: theme.spacing(1, 1, 1, 0),
-        // vertical padding + font size from searchIcon
-        paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-        transition: theme.transitions.create('width'),
-        width: '100%',
-        [theme.breakpoints.up('md')]: {
-            width: '20ch',
-        },
-    },
-}));
 
 const InsertNew = () => {
-    const classes = useStyles();
+    const [searchTerm, setSearchTerm] = useState('');
+    const [books, setBooks] = useState([]);
+    const baseURL = 'https://www.googleapis.com/books/v1/volumes'
+    const onInputChange = (e) => {
+        setSearchTerm(e.target.value);
+    }
+    React.useEffect(() => {
+        console.log(books);
+    }, [books]);
+    const searchBook = async () => {
+        const booksData = await axios.get(baseURL,
+            {
+                params: { q: searchTerm }
+            }
+        )
+        setBooks(booksData.data.items);
+    }
 
     return (
-        <div className={classes.root}>
-            <Container>
-                <AppBar position="static">
-                    <Toolbar>
-                        <Typography variant="h6" className={classes.title} noWrap>
-                            SEARCH
-                        </Typography>
-                        <div className={classes.search}>
-                            <div className={classes.searchIcon}>
-                                <SearchIcon />
-                            </div>
-                            <InputBase
-                                placeholder="Search…"
-                                classes={{
-                                    root: classes.inputRoot,
-                                    input: classes.inputInput,
-                                }}
-                                inputProps={{ 'aria-label': 'search' }}
-                            />
-                        </div>
-                        <Button variant="outlined" color="secondary" >GO</Button>
-                    </Toolbar>
-                </AppBar>
-            </Container>
+        <div>
+            <SearchArea handleSearch={onInputChange} searchBook={searchBook} />
+            <BookCard books={books} />
         </div>
     );
 }
---
+++ src/components/reusable/SearchArea.js
@@ -0,0 +1,101 @@
+import React, { useState } from 'react';
+import { makeStyles, fade } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container';
+import SearchIcon from '@material-ui/icons/Search';
+import InputBase from '@material-ui/core/InputBase';
+
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    title: {
+        display: 'none',
+        [theme.breakpoints.up('sm')]: {
+            display: 'block',
+        },
+    },
+    search: {
+        position: 'relative',
+        borderRadius: theme.shape.borderRadius,
+        backgroundColor: fade(theme.palette.common.white, 0.15),
+        '&:hover': {
+            backgroundColor: fade(theme.palette.common.white, 0.25),
+        },
+        marginRight: theme.spacing(2),
+        marginLeft: 0,
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            marginLeft: theme.spacing(3),
+            width: 'auto',
+        },
+    },
+    appbar: {
+        backgroundColor: "transparent"
+    },
+    searchIcon: {
+        padding: theme.spacing(0, 2),
+        height: '100%',
+        position: 'absolute',
+        pointerEvents: 'none',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    inputRoot: {
+        color: 'inherit',
+    },
+    inputInput: {
+        padding: theme.spacing(1, 1, 1, 0),
+        // vertical padding + font size from searchIcon
+        paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('md')]: {
+            width: '20ch',
+        },
+    },
+}));
+
+const SearchArea = ({ handleSearch, searchBook }) => {
+    const classes = useStyles();
+    return (
+        <div className={classes.root}>
+            <Container>
+                <AppBar position="static">
+                    <Toolbar>
+                        <Typography variant="h6" className={classes.title} noWrap>
+                            SEARCH
+                        </Typography>
+                        <div className={classes.search}>
+                            <div className={classes.searchIcon}>
+                                <SearchIcon />
+                            </div>
+                            <InputBase
+                                placeholder="Search…"
+                                classes={{
+                                    root: classes.inputRoot,
+                                    input: classes.inputInput,
+                                }}
+                                onChange={handleSearch}
+                                inputProps={{ 'aria-label': 'search' }}
+                            />
+                        </div>
+                        <Button
+                            variant="outlined"
+                            color="secondary"
+                            onClick={searchBook}
+                        >
+                            GO</Button>
+                        <Button variant="outlined" color="secondary" >CLEAR</Button>
+                    </Toolbar>
+                </AppBar>
+            </Container>
+        </div>
+    );
+}
+export default SearchArea;
